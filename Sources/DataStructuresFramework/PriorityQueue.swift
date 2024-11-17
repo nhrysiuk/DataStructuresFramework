769d79c0a125613e70ca8f72225218670bfbec2b
=nhrysiuk <=ngrysuk946@gmail.com>
2024-11-17 15:42:17 +0200

    make PriorityQueue public

--- Sources/DataStructuresFramework/PriorityQueue.swift
+++ Sources/DataStructuresFramework/PriorityQueue.swift
@@ -1,25 +1,25 @@
 import Foundation
 
 /// A priority queue that stores elements with their priorities.
-struct SimplePriorityQueue<Element: Comparable> {
+public struct SimplePriorityQueue<Element: Comparable> {
     private var elements: [(element: Element, priority: Int)] = []
 
     /// Checks if the queue is empty.
     /// - Returns: `true` if the queue is empty, `false` otherwise.
-    var isEmpty: Bool {
+    public var isEmpty: Bool {
         return elements.isEmpty
     }
 
     /// Adds an element to the queue with a specified priority.
     /// - Parameter element: The element to add.
     /// - Parameter priority: The priority of the element. Lower values mean higher priority.
-    mutating func enqueue(_ element: Element, withPriority priority: Int) {
+    public mutating func enqueue(_ element: Element, withPriority priority: Int) {
         elements.append((element, priority))
     }
 
     /// Removes and returns the element with the highest priority.
     /// - Returns: The element with the highest priority, or `nil` if the queue is empty.
-    mutating func dequeue() -> Element? {
+    public mutating func dequeue() -> Element? {
         guard !isEmpty else { return nil }
 
         if let index = elements.enumerated().min(by: { $0.element.priority < $1.element.priority })?.offset {
@@ -31,7 +31,7 @@ struct SimplePriorityQueue<Element: Comparable> {
 
     /// Returns the element with the highest priority without removing it.
     /// - Returns: The element with the highest priority, or `nil` if the queue is empty.
-    func peek() -> Element? {
+    public func peek() -> Element? {
         return elements.min(by: { $0.priority < $1.priority })?.element
     }
 }
---
+++ Tests/DataStructuresFrameworkTests/PriorityQueueTest.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import DataStructuresFramework
+
+struct PriorityQueueTest {
+
+    @Test func create() async throws {
+        
+    }
+
+}
